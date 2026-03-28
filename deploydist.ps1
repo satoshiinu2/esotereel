87c0fe5563a68ba4acd536c3fc5caf30206f65a2
satoshiinu2 <satoshiinu2@gmail.com>
2026-03-28 13:44:44 +0900

    fixed ps script

--- deploydist.ps1
+++ deploydist.ps1
@@ -4,7 +4,7 @@ if (Test-Path $dist) { Remove-Item $dist -Recurse -Force };
 New-Item -ItemType Directory -Path $dist;
 
 copy build/gui.exe $dist/;
-copy target/debug/nomyoedit_gui_helper.dll $dist/;
+copy target/release/nomyoedit_gui_helper.dll $dist/;
 copy build/x64/bin/libqt6advanceddocking.dll $dist/;
 
 cd $dist;
